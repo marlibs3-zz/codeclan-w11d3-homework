67e5069ad491b6c8f61696ad05aa2e4f53638f7a
Marta Beveridge <marta@beveridge.uk>
2018-05-16 16:30:52 +0100

    12/12 tests passing

--- models/traveller.js
+++ models/traveller.js
@@ -3,31 +3,43 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  const startLocations = this.journeys.map(journey => journey.startLocation);
+  return startLocations;
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  const endLocations = this.journeys.map(journey => journey.endLocation);
+  return endLocations;
 };
 
 Traveller.prototype.getModesOfTransport = function () {
-
+  const modesOfTransport = this.journeys.map(journey => journey.transport);
+  return modesOfTransport;
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  const journeyByTransport = this.journeys.filter(journey => journey.transport === transport);
+  return journeyByTransport;
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  const journeyByDistance = this.journeys.filter(journey => journey.distance >= minDistance);
+  return journeyByDistance;
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  const totalDistance = this.journeys.reduce((total, journey) => {
+    return total += journey.distance;
+  }, 0);
+  return totalDistance;
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-  
+  const allModesOfTransport = this.getModesOfTransport();
+  uniqueArray = allModesOfTransport.filter(function(mode, pos) {
+    return allModesOfTransport.indexOf(mode) == pos;
+  })
+  return uniqueArray;
 };
 
 
